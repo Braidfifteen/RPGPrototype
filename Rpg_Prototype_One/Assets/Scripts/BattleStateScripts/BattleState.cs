f0628d348ea8c2da9ed6cbf9ab32efb0d01516f4
Roy Atkins <royanthony0522@gmail.com>
2017-01-15 13:11:09 -0500

    Random number of enemies with random sprites working.

--- Rpg_Prototype_One/Assets/Scripts/BattleStateScripts/BattleState.cs
+++ Rpg_Prototype_One/Assets/Scripts/BattleStateScripts/BattleState.cs
@@ -4,9 +4,25 @@ public class BattleState : MonoBehaviour, IState
 {
     public StateMachine gameStateManager;
 
+    private IUpdatable[] updateObjects;
+    private IInput[] inputObjects;
+    private IExitable[] exitObjects;
+    private IEnterable[] enterObjects;
+
+    private void Awake()
+    {
+        inputObjects = GetComponentsInChildren<IInput>();
+        updateObjects = GetComponentsInChildren<IUpdatable>();
+        exitObjects = GetComponentsInChildren<IExitable>();
+        enterObjects = GetComponentsInChildren<IEnterable>();
+    }
+
     public void OnUpdate()
     {
-
+        for (int i = 0; i < updateObjects.Length; i++)
+        {
+            updateObjects[i].OnUpdate();
+        }
     }
 
     public void OnEnter()
@@ -14,6 +30,11 @@ public class BattleState : MonoBehaviour, IState
         if (!transform.gameObject.activeInHierarchy)
         {
             transform.gameObject.SetActive(true);
+        }
+
+        for (int i = 0; i < enterObjects.Length; i++)
+        {
+            enterObjects[i].OnEnter();
         }
     }
 
@@ -23,6 +44,11 @@ public class BattleState : MonoBehaviour, IState
         {
             transform.gameObject.SetActive(false);
         }
+
+        for (int i = 0; i < exitObjects.Length; i++)
+        {
+            exitObjects[i].OnExit();
+        }
     }
 
     public void HandleInput()
@@ -30,6 +56,11 @@ public class BattleState : MonoBehaviour, IState
         if (Input.GetKeyDown(KeyCode.Return))
         {
             gameStateManager.ChangeState("World Map State (WorldMapState)");
+        }
+
+        for (int i = 0; i < inputObjects.Length; i++)
+        {
+            inputObjects[i].GetInput();
         }
     }
 }
---
+++ Rpg_Prototype_One/Assets/Scripts/BattleStateScripts/CharactersInBattleState.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class CharactersInBattleState : MonoBehaviour, IEnterable, IExitable
+{
+    public GameObject[] enemySpots;
+    //public GameObject[] playerSpots;
+
+    public Sprite[] testEnemySprites;
+
+    private SpriteRenderer enemeySprite;
+
+
+    public void OnEnter()
+    {
+        for (int i = 0; i < Random.Range(1, enemySpots.Length + 1); i++)
+        {
+            enemeySprite = enemySpots[i].GetComponent<SpriteRenderer>();
+            enemeySprite.sprite = testEnemySprites[Random.Range(0, testEnemySprites.Length)];
+            enemySpots[i].SetActive(true);
+        }
+    }
+
+    public void OnExit()
+    {
+        for (int i = 0; i < enemySpots.Length; i++)
+        {
+            enemeySprite = enemySpots[i].GetComponent<SpriteRenderer>();
+            enemeySprite.sprite = null;
+            enemySpots[i].SetActive(false);
+        }
+    }
+}
