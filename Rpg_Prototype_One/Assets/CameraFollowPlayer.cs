2b922c342097e2337986ca063ec82672ea95e9e4
Roy Atkins <royanthony0522@gmail.com>
2017-01-14 21:43:36 -0500

    .

--- Rpg_Prototype_One/Assets/CameraFollowPlayer.cs
+++ Rpg_Prototype_One/Assets/CameraFollowPlayer.cs
@@ -1,39 +1,26 @@
 ﻿using UnityEngine;
 
-public class CameraFollowPlayer : MonoBehaviour
+public class CameraFollowPlayer : MonoBehaviour, IUpdatable
 {
     public GameObject player;
+    public Camera cam;
+
     private float mapWidth = 16.0f;
     private float maxX = 14.4f;
     private float minX = 1.6f;
     private float maxY = -1.2f;
     private float minY = -14.8f;
 
-    private void Start()
-    {
-       // calcMinMax();
-    }
-
-    private void Update()
+    public void OnUpdate()
     {
         Vector3 newPos = new Vector3(player.transform.position.x,
-                player.transform.position.y, transform.position.z);
+                player.transform.position.y, cam.transform.position.z);
 
-      //  newPos = Camera.main.ScreenToWorldPoint(newPos);
 
         newPos.x = Mathf.Clamp(newPos.x, minX, maxX);
         newPos.y = Mathf.Clamp(newPos.y, minY, maxY);
 
-        transform.position = newPos;
+        cam.transform.position = newPos;
     }
 
-    private void calcMinMax()
-    {
-        float height = Camera.main.orthographicSize * 2.0f;
-        print(height);
-        float width = height * Screen.width / Screen.height;
-        maxX = (mapWidth - width) / 2.0f;
-        minX = -maxX;
-        print(minX);
-    }
 }
