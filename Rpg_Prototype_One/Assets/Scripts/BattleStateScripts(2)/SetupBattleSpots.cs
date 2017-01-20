76b3c4e11e58ee284dfd70b7e60948b96352bc15
Roy Atkins <royanthony0522@gmail.com>
2017-01-20 18:34:09 -0500

    Reworking the battle state.

---
+++ Rpg_Prototype_One/Assets/Scripts/BattleStateScripts(2)/SetupBattleSpots.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+// This script will be attached to Battle State Enter gameObject. It inits the character spots for the battle.
+public class SetupBattleSpots : MonoBehaviour, IEnterable, IExitable, ILateEnter
+{
+    public GameObject[] playerPartySpots;
+    public CharacterSpot[] enemyCharacterSpots;
+    public GameObject[] enemyGameObjectSpots;
+
+    // This list of enemy prefabs will eventually come from a region class which will have different enemies
+    // depending on the region.
+    public GameObject[] enemyPrefabs;
+
+    private List<GameObject> activeSpots = new List<GameObject>();
+
+    // This script is ISubject
+    private int currentIndex;
+
+    public void OnEnter()
+    {
+        setupEnemySpots();
+    }
+    
+    public void OnLateEnter()
+    {
+        getActiveSpots();
+        //Notify() -- implemented ISubject
+    }
+
+    public void OnExit()
+    {
+        activeSpots.Clear();
+    }
+
+    // This method is called in OnEnter(), BEFORE getActiveSpots() which is called in OnLateEnter()
+    // Finding the enemey/player party count activates only the necessary spots, which getActiveSpots()
+    // puts into a list.
+    private void setupEnemySpots()
+    {
+        for (int i = 0; i < Random.Range(1, enemyCharacterSpots.Length + 1); i++)
+        {
+            GameObject enemy = enemyPrefabs[Random.Range(0, enemyPrefabs.Length)];
+            Sprite enemySprite = enemy.GetComponent<SpriteRenderer>().sprite;
+            CharacterInfo enemyInfo = enemy.GetComponent<CharacterInfo>();
+
+            enemyCharacterSpots[i].Set(enemySprite, enemyInfo);
+        }
+    }
+
+    private void getActiveSpots()
+    {
+        for (int i = 0; i < playerPartySpots.Length; i++)
+        {
+            if (playerPartySpots[i].activeInHierarchy)
+                activeSpots.Add(playerPartySpots[i]);
+        }
+
+        for (int i = 0; i < enemyGameObjectSpots.Length; i++)
+        {
+            if (enemyGameObjectSpots[i].activeInHierarchy)
+                activeSpots.Add(enemyGameObjectSpots[i]);
+        }
+    }
+}
---
+++ Rpg_Prototype_One/Assets/Scripts/BattleStateScripts(2)/StateParentScript.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+// This script will be attached to the parent game object of each individual state. It will control the flow of that specific
+// state.
+public class StateParentScript : MonoBehaviour, IState
+{
+    public SubStateMachine battleStateManager;
+
+    // This is used if needed
+    public StateMachine gameStateManager;
+
+    public bool SetActiveOnEnterAndExit = true;
+
+    private IUpdatable[] updateObjects;
+    private IInput[] inputObjects;
+    private IExitable[] exitObjects;
+    private IEnterable[] enterObjects;
+    private ILateEnter[] lateEnterObjects;
+
+    private void Awake()
+    {
+        inputObjects = GetComponentsInChildren<IInput>();
+        updateObjects = GetComponentsInChildren<IUpdatable>();
+        exitObjects = GetComponentsInChildren<IExitable>();
+        enterObjects = GetComponentsInChildren<IEnterable>();
+        lateEnterObjects = GetComponentsInChildren<ILateEnter>();
+    }
+
+    public void OnUpdate()
+    {
+        for (int i = 0; i < updateObjects.Length; i++)
+            updateObjects[i].OnUpdate();
+    }
+
+    public void OnEnter()
+    {
+        if (SetActiveOnEnterAndExit)
+        {
+            if (!transform.gameObject.activeInHierarchy)
+                transform.gameObject.SetActive(true);
+        }
+
+        for (int i = 0; i < enterObjects.Length; i++)
+            enterObjects[i].OnEnter();
+
+        for (int i = 0; i < lateEnterObjects.Length; i++)
+            lateEnterObjects[i].OnLateEnter();
+    }
+
+    public void OnExit()
+    {
+        for (int i = 0; i < exitObjects.Length; i++)
+            exitObjects[i].OnExit();
+
+        if (SetActiveOnEnterAndExit)
+        {
+            if (transform.gameObject.activeInHierarchy)
+                transform.gameObject.SetActive(false);
+        }
+    }
+
+    public void HandleInput()
+    {
+        for (int i = 0; i < inputObjects.Length; i++)
+            inputObjects[i].GetInput();
+    }
+}
