b53594a6b29b40cc45866827f50e8ea813139d06
Roy Atkins <royanthony0522@gmail.com>
2017-01-21 15:43:11 -0500

    Just added some comments about the program control flow.

--- Rpg_Prototype_One/Assets/Scripts/State Scripts/StateMachine.cs
+++ Rpg_Prototype_One/Assets/Scripts/State Scripts/StateMachine.cs
@@ -2,6 +2,16 @@
 using System;
 using System.Collections.Generic;
 
+//
+// This script is attached to the Game State Manager. It controls the flow of the entire program and is the only
+// script which unity calls Update() on, in which this script calls currentStates OnUpdate().
+//
+// The order of script calls is as follows:
+// -- Game State Manager/StateMachine calls either a SubStateMachine or a StateParentScript.
+// StateParentScript is attached to the parent gameObject, which each state has. StateParenScript has OnUpdate(), OnEnter(), OnExit(), etc, which will call the child gameObjects IUpdatable, IEnterable, IExitable, etc.
+// SubStateMachine has the same funcionality as StateParentScript, except that it is also it's own state machine. This Script is used for the parent gameObject of a state if the state needs to keep track of it's own states, and child gameObjects of this state will use StateParentScript which will call IUpdatable, IEnterable, IExitable in its child gameObjects. 
+//
+
 public class StateMachine : MonoBehaviour
 {
     private Dictionary<string, IState> stateDict = new Dictionary<string, IState>();
@@ -17,17 +27,13 @@ public class StateMachine : MonoBehaviour
                 IState state = objectsWithState[i].GetComponent<IState>();
                 string type = state.ToString();
                 stateDict.Add(type, state);
-
             }
             catch (NullReferenceException)
             {
                 continue;
             }
         }
-
         currentState = stateDict[objectsWithState[0].GetComponent<IState>().ToString()];
-
-
     }
 
     private void Update()
